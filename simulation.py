c6a3d4e745ec117c5331e588099dc6750ddf80e8
Tate P. Osborne <tate0301@gmail.com>
2023-02-18 16:23:13 -0500

    got general bulk of cache simulation logic in

--- simulation.py
+++ simulation.py
@@ -3,34 +3,6 @@ from Cache import Cache
 import constants as c
 
 
-# # compute the block offset, index, and tag
-#     block_offset = address & 63
-#     index = (address >> 6) & 15
-#     tag = (address >> 10) & 63
-#     # iterate over all of the sets in the cache until at the set that matches the index
-#     for s in cache.sets:
-#         if s == index:
-#             # iterate over all of the blocks in this set (this is just one block for direct-mapped)
-#             for b in s.blocks:
-#                 # check if the tag in the block matches the tag from the address and the valid flag is true
-#                 if b.tag == tag and b.valid:
-#                     # CACHE HIT
-#                     # read the word at this address and return it
-#                     block_address = address / c.CACHE_BLOCK_SIZE
-                    
-#                     b.tag = tag
-#                     b.valid = True
-#                     # update the rotation of the tag queue using LRU
-#                     for j, t in enumerate(s.tag_queue):
-#                         if t == tag:
-#                             for k in range(j, len(s.tag_queue)-1):
-#                                 s.tag_queue[k] = s.tag.queue[k+1]     
-#                             s.tag_queue[-1] = tag
-#                     return word
-#                 else:
-#                     # CACHE MISS
-#                     pass
-
 def read_word(address):
     """
     Check for the requested address in the cache for the word
@@ -43,15 +15,65 @@ def read_word(address):
     Returns:
         word: the requested word
     """
-    pass
+    outcome = ""
+    
+    # compute variables from address for cache/memory access
+    offset = address % c.NUM_SETS
+    index = address // c.CACHE_BLOCK_SIZE
+    tag = (address >> 10) & 63
+    start_address = index * c.CACHE_BLOCK_SIZE
+    
+    # go to the index in the cache where the address would be if in the cache
+    target_set = cache.sets[index]
+    # for a direct-mapped cache, there is one block in each set
+    target_block = target_set[0]
+    # check if the tag in this block in the cache
+    if target_block.tag == tag:
+        # this is cache hit, so retrieve the data from the cache and set hit flag
+        word = target_block.data[offset]
+        outcome = "hit"
+    else:
+        # this is a cache miss, so go to memory for the value
+        word = memory[address] + 256*memory[address+1] + 256*256*memory[address+2] + 256*256*256*memory[address+3]
+        # load the block into the cache
+        # iterate through memory starting at the start address for the block
+        for i in range(64):
+            byte = 8*i
+            # store the block in the cache
+            target_block.data[i] = memory[start_address+(byte)]
+                
+        # update the tag queue, which is going to have one element for direct-mapped
+        target_set.tag_queue[0] = tag
+        # set the valid flag so we know the cache is loaded accurately
+        target_block.valid = True
+        outcome = "miss"
+    
+    print(f"read {outcome} [ addr={address} index={index} tag={tag}: word={word} ({start_address} - {start_address + c.CACHE_BLOCK_SIZE - 1}) ]")
+    print(f"{target_set.tag_queue}")
+    
+    return word
 
+
+##########################
+# ***run the program *** #
+##########################
 
 # initialize memory and cache
-memory = [0 for i in range(c.MEMORY_SIZE)]
+memory = bytearray(c.MEMORY_SIZE)
 cache = Cache(c.NUM_SETS, c.ASSOCIATIVITY, c.CACHE_BLOCK_SIZE)
 
-# simulate the cache with read operations
-read_word(46916)
+# print cache parameters
+print("--------------------------------")
+print(f"cache size: {c.CACHE_SIZE}")
+print(f"block size: {c.CACHE_BLOCK_SIZE}")
+print(f"num blocks: {c.NUM_BLOCKS}")
+print(f"num sets: {c.NUM_SETS}")
+print(f"associativity: {c.ASSOCIATIVITY}")
+print(f"tag length: {c.TAG_LENGTH}")
+print("--------------------------------")
+
+# simulate the cache with read operations and print output
+word = read_word(46916)
 read_word(46932)
 read_word(12936)
 read_word(13964)
