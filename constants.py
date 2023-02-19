e40bda188d218d67127b58c0c49a811ae7bde5a3
Tate P. Osborne <tate0301@gmail.com>
2023-02-18 22:07:30 -0500

    adding new constant for organization

--- constants.py
+++ constants.py
@@ -5,4 +5,5 @@ CACHE_SIZE       = 1024  # 2^10
 CACHE_BLOCK_SIZE = 64  # 2^6
 NUM_BLOCKS = CACHE_SIZE // CACHE_BLOCK_SIZE
 NUM_SETS = NUM_BLOCKS // ASSOCIATIVITY
-TAG_LENGTH = 6  # address length (16) - index length (4) - offset length (6)
+TAG_LENGTH = MEMORY_SIZE // CACHE_SIZE
+MAX_BYTE = 255 # ranges from 0-255
